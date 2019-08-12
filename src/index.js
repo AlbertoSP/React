e5d59bf94352197bbe2de0635e1a089bebc94b7f
Alberto Sánchez Plaza <alberto@MacBook-Air-de-Alberto.local>
2019-08-12 12:37:29 +0200

    hello world basic

--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
-const require = require('express');
+const express = require('express');
+
+const morgan = require('morgan');
 const app = express();
+//configuracion
+app.set('port', process.env.PORT || 3000)
+//middlewares
+app.use(morgan('dev'));
+app.use(express.json());
 
 
-app.listen(3000,  () => {
-    
+//rutas
+
+app.use(require('./routes/tareas.routes'))
+//static files
+
+
+
+
+//empezando servidor
+app.listen(app.get('port'),  () => {
+    console.log(`Server on port ${app.get('port')}`);
 })
