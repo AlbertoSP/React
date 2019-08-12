d6eafdcea517b385a4b1e2595537c92c09c533f2
Alberto Sánchez Plaza <alberto@MacBook-Air-de-Alberto.local>
2019-08-12 15:49:52 +0200

    get, post, put , delete añadido

--- src/routes/tareas.routes.js
+++ src/routes/tareas.routes.js
@@ -1,22 +1,31 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Task = require('../models/task')
+const Task = require("../models/task");
 
 
+router.get("/", async (req, res) => {
+  const tasks = await Task.find();
+  res.json(tasks);
+});
 
+router.post("/", async (req, res) => {
+  const { title, description } = req.body;
+  const task = new Task({ title, description });
+  await task.save();
+  console.log(task);
+  res.json({ status: "tarea guardada" });
+});
 
+router.put("/:id", async (req, res) => {
+  const { title, description } = req.body;
+  const newTask = { title, description };
+  await Task.findByIdAndUpdate(req.params.id, newTask);
+  res.json({ status: "tarea actualizada" });
+});
 
-router.get('/', async (req, res) => {
-    const tasks = await Task.find();
-    res.json(tasks);
-})
+router.delete("/:id", async (req, res) => {
+  await Task.findByIdAndDelete(req.params.id);
+  res.json({ status: "tarea eliminada" });
+});
 
-router.post('/', async (req, res) => {
-    const { title, description } = req.body;
-    const task = new Task ({title, description});
-    await task.save();
-    console.log(task);
-    res.json({status: 'tarea guardada'});
-
-})
 module.exports = router;
