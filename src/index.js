d833258775b1860c76f907604bdc491452f3d506
Alberto Sánchez Plaza <alberto@MacBook-Air-de-Alberto.local>
2019-08-12 16:54:27 +0200

    back preparado

--- src/index.js
+++ src/index.js
@@ -1,29 +1,23 @@
-const express = require('express');
-
-const morgan = require('morgan');
-const path = require('path');
-const { mongoose } = require('./database')
+const express = require("express");
+const morgan = require("morgan");
+const path = require("path");
+const { mongoose } = require("./database");
 const app = express();
 
-
 //configuracion
-app.set('port', process.env.PORT || 3000)
+app.set("port", process.env.PORT || 3000);
+
 //middlewares
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(express.json());
 
-
 //rutas
-
-app.use('/api/tasks', require('./routes/tareas.routes'));
+app.use("/api/tasks", require("./routes/tareas.routes"));
 
 //static files
-
-app.use(express.static(path.join(__dirname, 'public')));
-
-
+app.use(express.static(path.join(__dirname, "public")));
 
 //empezando servidor
-app.listen(app.get('port'),  () => {
-    console.log(`Server on port ${app.get('port')}`);
-})
+app.listen(app.get("port"), () => {
+  console.log(`Server on port ${app.get("port")}`);
+});
